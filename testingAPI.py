fcd0c9b44895eb0b5b0451b416fcd1bca9de5587
OD <email>
2024-05-10 19:32:09 -0400

    update

--- testingAPI.py
+++ testingAPI.py
@@ -1,5 +1,5 @@
 import requests
 
-url = "https://api.github.com/repos/rsapkf/42/commits"
+url = "  https://api.github.com/repos/ocaldwell1/codingStuff/commits"
 headers = {"Accept": "application/vnd.github.inertia-preview+json"}
 r = requests.get(url, headers=headers)
